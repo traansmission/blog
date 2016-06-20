074376df07d3545256b3639aca158c6522770666
emillamm <e_millerkiller@hotmail.com>
2016-06-20 17:16:46 -0400

    added our own navbar

--- core/built/assets/ghost.js
+++ core/built/assets/ghost.js
@@ -35680,7 +35680,7 @@ catch(err) {
 if (runningTests) {
   require("ghost/tests/test-helper");
 } else {
-  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0+2cfc46d5"});
+  require("ghost/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ghost","version":"0.0.0+a2f45052"});
 }
 
 /* jshint ignore:end */
